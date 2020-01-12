ce7ff0008e46f3eb4ae51a21039f1e8d68deec04
ruslanofc <ruslanofcourse@gmail.com>
2020-01-12 14:01:29 +0300

    added a new packeges

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
+
 import * as serviceWorker from './serviceWorker';
+//import { ReactComponent } from '*.svg';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
