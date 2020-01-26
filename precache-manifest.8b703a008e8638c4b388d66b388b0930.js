e2f49c0fa5add360ff836b5b0977124b73df4253
ruslanofc <ruslanofcourse@gmail.com>
2020-01-26 20:05:45 +0300

    Updates

---
+++ precache-manifest.8b703a008e8638c4b388d66b388b0930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6eec906cc09255019c6529f27900c0",
+    "url": "/git_client/index.html"
+  },
+  {
+    "revision": "a86293b04d18b15fc0b8",
+    "url": "/git_client/static/css/main.58476b5c.chunk.css"
+  },
+  {
+    "revision": "5439683abfdf1f9a7a48",
+    "url": "/git_client/static/js/2.cfac3f21.chunk.js"
+  },
+  {
+    "revision": "02fb7f6b2676f63cbfba450afc3f216f",
+    "url": "/git_client/static/js/2.cfac3f21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a86293b04d18b15fc0b8",
+    "url": "/git_client/static/js/main.ff6e4719.chunk.js"
+  },
+  {
+    "revision": "62bdf6e705701d908932",
+    "url": "/git_client/static/js/runtime-main.dfe637ca.js"
+  }
+]);
